58cb267744afecb1a14637a16b67aaca1a63988b
Chris Ballard <chrisballard@Chriss-MacBook-Air.local>
2025-12-11 17:39:26 -0600

    Setup dev environment: Update App Name to STS (Dev)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.cbxnexus.scantosold',
-  appName: 'Scan To Sold',
+  appName: 'Scan To Sold (Dev)',
   webDir: 'dist'
 };
 
